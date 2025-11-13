ac1d5866db49c96d758467e51485ef2c84512ea2
GibbedBoomer <mmm04302004@gmail.com>
2025-11-13 11:14:29 -0500

    set up draw and display for cards by adapting teacher work and using some logic trouble shooting to get cards to display right. Made set up part of an initialize loop for future levels to be possible to set up.

--- code.js
+++ code.js
@@ -5,16 +5,49 @@ let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
-let colors = ["red", "blue", "yellow", "green", "purple", "orange", "pink", "brown"];
-let cardSpot =[
+function gameInitialize() {
+    let numberOfColors = 8;
+    let colors = [];
 
-];
-for (let i = 0; i < colors.length; i++) {
-    cardSpot.push(colors[i])
-    cardSpot.push(colors[i])
-};
+    for (let i = 0; i < numberOfColors; i++){
+        let colorAdd = toolbox.getRandomColor();
+        colors.push(colorAdd)
+    }
+    console.log(colors)
 
-cardSpot = toolbox.shuffleArray(cardSpot)
+    let cardSpot = [];
+
+    for (let i = 0; i < colors.length; i++) {
+        cardSpot.push(colors[i])
+        cardSpot.push(colors[i])
+    };
+
+    cardSpot = toolbox.shuffleArray(cardSpot)
+    console.log(cardSpot)
+    
+    let cardX = 50;
+    let cardY = 50;
+    let cardsPerRow = 8;
+    let cardSpacing = 150;
+
+    for (let i = 0; i < cardSpot.length; i++) {
+        // Draw the card
+        let cardDraw = new Card(canvas, pencil, cardX, cardY, cardSpot[i]);
+        cardDraw.draw();
+
+        
+        cardX += cardSpacing;
+
+        // Check if we’ve reached the end of a row
+        if ((i + 1) % cardsPerRow === 0) {
+            cardX = 50; // reset to first X
+            cardY += 200; // move down next row
+        }
+}
+    
+}
+
+gameInitialize()
 
 function gameLoop() {
 
