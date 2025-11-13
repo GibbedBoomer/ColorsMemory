bc758a3e3f06bf57926e5832acf57e54ab59a55e
GibbedBoomer <mmm04302004@gmail.com>
2025-11-13 10:12:35 -0500

    set up randomizing position, started card class to begin working on actually drawing the cards to the position set, also made gameloop

--- code.js
+++ code.js
@@ -1,8 +1,21 @@
 import { Toolbox } from "./toolbox.js";
+import { Card } from "./card.js";
 
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
-let colors = [red, blue, yellow, green, purple, orange, pink, brown]
+let colors = ["red", "blue", "yellow", "green", "purple", "orange", "pink", "brown"];
+let cardSpot =[
 
+];
+for (let i = 0; i < colors.length; i++) {
+    cardSpot.push(colors[i])
+    cardSpot.push(colors[i])
+};
+
+cardSpot = toolbox.shuffleArray(cardSpot)
+
+function gameLoop() {
+
+};
